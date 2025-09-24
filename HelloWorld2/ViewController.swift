76a099a2a51cfee82e4f451be1d7e3ad0334578b
OlgVish <leshaowl@gmail.com>
2025-09-24 13:13:50 +0300

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Olga Vishnevskaia on 24/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
