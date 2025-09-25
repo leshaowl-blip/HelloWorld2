538ba767f21dc063bec4564549d8b20971e67dec
OlgVish <leshaowl@gmail.com>
2025-09-25 13:53:19 +0300

    Setup UI

--- HelloWorld2/ViewController.swift
+++ HelloWorld2/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
